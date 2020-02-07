d2eadb25a9257b924ceebca6c30193937f7fc885
Karina Gaulin <karina@domandtom.com>
2020-02-07 09:15:53 -0500

    Basic setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const { PORT } = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
+
+module.exprorts = {app};
