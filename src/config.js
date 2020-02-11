2702b77eec689d7ab44c2377fd23bb83a5478724
Karina Gaulin <karina@domandtom.com>
2020-02-11 11:48:45 -0500

    change config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  API_BASE_URL: process.env.REACT_APP_BASE_URL,
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://karinagaulin@localhost/inspiration-cloud',
 }
