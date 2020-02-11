daa51e455be5fb02953b0ffc233b51685cdc11c9
Karina Gaulin <karina@domandtom.com>
2020-02-11 11:26:12 -0500

    Update for production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://karinagaulin@localhost/inspiration-cloud',
 }
